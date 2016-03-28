a8cd6e6973de049399cbf1504ad75624894ac9b3
Damien Dart <damiendart@pobox.com>
2016-03-28 11:27:13 +0100

    "setup.py": Fix and update trove classifers.

--- setup.py
+++ setup.py
@@ -13,20 +13,21 @@ import nfsnapi
 setup(
   author = "Damien Dart",
   author_email = "damiendart@pobox.com",
-  classifiers = (
+  classifiers = [
     "Development Status :: 3 - Alpha",
     "Intended Audience :: Developers",
     "License :: Public Domain",
     "Programming Language :: Python",
-    # TODO: Test "nfsnapi" with other versions of Python.
+    "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Operating System :: OS Independent",
     "Topic :: Internet :: WWW/HTTP",
     "Topic :: Software Development :: Libraries :: Python Modules"
-  ),
+  ],
   description = nfsnapi.__doc__.split("\n", 1)[0],
   keywords = "nearlyfreespeech nfsn",
   long_description = open("README.txt").read(),
